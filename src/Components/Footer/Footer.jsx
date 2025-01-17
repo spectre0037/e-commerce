00075a921db747afe2cc695c04e7d7f20c0c3970
Abdullah Bin Zubair <u2023037@giki.edu.pk>
2025-01-17 16:27:39 +0500

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
             <div className="container-fluid footer-block mt-5">
                 <div className="row d-flex justify-content-around align-content-center align-items-center">
                     <div className="col-xl-4 ">
-                        <h3 className='text text-center'>CONTACT US</h3>
+                        <h3 className=' text text-center'>CONTACT US</h3>
                         <div className='contact-us-text ps-3 mt-3'>
-                            <h3>AZ UTAMA RESOURCES</h3>
+                            <p>AZ UTAMA RESOURCES</p>
                             <p>20A JALAN MEDAN 9 TAMAN MEDAN BARU 46000</p>
                             <p>PETALING JAYA SELANGOR</p>
                             <p>email : azutamaresources@gmail.com</p>
@@ -25,8 +25,10 @@ const Footer = () => {
                         <p className='text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus totam quae reprehenderit vel illo aperiam dolorum! Blanditiis accusantium nisi cum!</p>
                     </div>
                     <div className="col-xl-2 d-flex flex-column justify-content-center">
-                        <img className='p-0 m-0 ' src={logo} alt="" height={200}/>
-                        <p className='text-center m-0'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus totam quae reprehenderit vel illo aperiam dolorum! Blanditiis accusantium nisi cum!</p>
+                        <img className='p-0 m-0 footer-logo' src={logo} alt="" height={200}/>
+                        <p className='text-center m-0'>
+                            2025 . all rights reserved
+                        </p>
                     </div>
                 </div>
             </div>
