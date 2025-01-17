f588cff4a2dbdbd3bbdeb0f2089c9cf890b1feff
Abdullah Bin Zubair <u2023037@giki.edu.pk>
2025-01-17 17:49:55 +0500

    Update ReadyMadeCurtains.jsx

--- src/PAGE/ReadyMadeCurtains/ReadyMadeCurtains.jsx
+++ src/PAGE/ReadyMadeCurtains/ReadyMadeCurtains.jsx
@@ -9,7 +9,7 @@ import Footer from '../../Components/Footer/Footer'
 const ReadyMadeCurtains = () => {
     return (
         <div>
-            <div className='p-0 m-0'>
+            <div className='Ready-made-curtains p-0 m-0'>
                 <Navbar />
                 <div className='ready-made-curtain-image p-0 m-0'>
                 </div>
