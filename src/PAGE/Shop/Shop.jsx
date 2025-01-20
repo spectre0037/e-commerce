6b451036dc1f3e7f30e201a09f2173aefc9c2b47
Abdullah Bin Zubair <u2023037@giki.edu.pk>
2025-01-20 14:58:03 +0500

    Update Shop.jsx

--- src/PAGE/Shop/Shop.jsx
+++ src/PAGE/Shop/Shop.jsx
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import './ContactUs.css';
+import './Shop.css';
 import Navbar from '../../Components/Navbar/Navbar';
 
-const ContactUs = () => {
+const Shop = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs
-            .sendForm('service_zrwxrlc', 'template_57p3fmp', form.current, {
+            .sendForm('service_zrwxrlc', 'template_rhrxqph', form.current, {
                 publicKey: 'EGs920113ObrXOfvo',
             })
             .then(
@@ -33,16 +33,20 @@ const ContactUs = () => {
                 ref={form}
                 onSubmit={sendEmail}
             >
-                <div className="form-control bg-body-tertiary p-4 rounded-4 border d-flex justify-content-center flex-column">
-                    <h1 className="text text-center">CONTACT US</h1>
+                <div className=" form-contorll bg-body-tertiary p-4 rounded-4 d-flex justify-content-center flex-column">
+                    <h1 className="text text-center">SHOP NOW</h1>
                     <label>Name</label>
-                    <input type="text" name="from_name" placeholder="name" />
+                    <input type="text" name="from_name" placeholder="Name" />
+                    <label>Item id</label>
+                    <input type="text" name="item_id" placeholder="xxxx" />
                     <label>Email</label>
                     <input type="email" name="from_email" placeholder="name@example.com" />
                     <label>Contact Number</label>
                     <input type="contact" name="from_contact" />
-                    <label>Message</label>
-                    <textarea name="message" />
+                    <label>Delivery Address</label>
+                    <input name="Delivery_address" />
+                    <label>Transaction Id</label>
+                    <input name="Transaction_id" />
                     <input className="button" type="submit" value="Send" />
                 </div>
             </form>
@@ -50,4 +54,4 @@ const ContactUs = () => {
     );
 };
 
-export default ContactUs;
+export default Shop;
