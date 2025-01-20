899beb3f856f3798af7a427d05eb699ad5951768
Abdullah Bin Zubair <u2023037@giki.edu.pk>
2025-01-20 14:57:37 +0500

    Update Shop.jsx

--- src/PAGE/Shop/Shop.jsx
+++ src/PAGE/Shop/Shop.jsx
@@ -1,16 +1,18 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import './Shop.css';
+import './ContactUs.css';
 import Navbar from '../../Components/Navbar/Navbar';
 
-const Shop = () => {
+const ContactUs = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs
-            .sendForm('service_vw4hnrb', 'template_ay9liav', form.current, 's7gzIzJoCQqtshaEx')
+            .sendForm('service_zrwxrlc', 'template_57p3fmp', form.current, {
+                publicKey: 'EGs920113ObrXOfvo',
+            })
             .then(
                 () => {
                     console.log('SUCCESS!');
@@ -18,7 +20,7 @@ const Shop = () => {
                     form.current.reset();
                 },
                 (error) => {
-                    console.error('FAILED...', error.text);
+                    console.log('FAILED...', error.text);
                 }
             );
     };
@@ -31,68 +33,21 @@ const Shop = () => {
                 ref={form}
                 onSubmit={sendEmail}
             >
-                <div className="form-control bg-body-tertiary p-4 rounded-4 d-flex justify-content-center flex-column">
-                    <h1 className="text text-center">SHOP NOW</h1>
-
-                    <label htmlFor="from_name">Name</label>
-                    <input
-                        type="text"
-                        id="from_name"
-                        name="from_name"
-                        placeholder="Your Name"
-                        required
-                    />
-
-                    <label htmlFor="item_id">Item ID</label>
-                    <input
-                        type="text"
-                        id="item_id"
-                        name="item_id"
-                        placeholder="xxxx"
-                        required
-                    />
-
-                    <label htmlFor="from_email">Email</label>
-                    <input
-                        type="email"
-                        id="from_email"
-                        name="from_email"
-                        placeholder="name@example.com"
-                        required
-                    />
-
-                    <label htmlFor="from_contact">Contact Number</label>
-                    <input
-                        type="tel"
-                        id="from_contact"
-                        name="from_contact"
-                        placeholder="Your contact number"
-                        required
-                    />
-
-                    <label htmlFor="Delivery_address">Delivery Address</label>
-                    <input
-                        type="text"
-                        id="Delivery_address"
-                        name="Delivery_address"
-                        placeholder="Your delivery address"
-                        required
-                    />
-
-                    <label htmlFor="Transaction_id">Transaction ID</label>
-                    <input
-                        type="text"
-                        id="Transaction_id"
-                        name="Transaction_id"
-                        placeholder="Your transaction ID"
-                        required
-                    />
-
-                    <input className="button mt-3" type="submit" value="Send" />
+                <div className="form-control bg-body-tertiary p-4 rounded-4 border d-flex justify-content-center flex-column">
+                    <h1 className="text text-center">CONTACT US</h1>
+                    <label>Name</label>
+                    <input type="text" name="from_name" placeholder="name" />
+                    <label>Email</label>
+                    <input type="email" name="from_email" placeholder="name@example.com" />
+                    <label>Contact Number</label>
+                    <input type="contact" name="from_contact" />
+                    <label>Message</label>
+                    <textarea name="message" />
+                    <input className="button" type="submit" value="Send" />
                 </div>
             </form>
         </div>
     );
 };
 
-export default Shop;
+export default ContactUs;
